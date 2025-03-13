e5e1843ce33b60987f1b501c8643906c77b03e46
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-03-13 06:01:13 -0700

    finished part-1

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,116 @@
+// const country = 'Canada'
+// const continent = 'North America'
+// let population = 30
+// const isIsland = false
+// let language = 'English'
+
+// console.log(country)
+// console.log(continent)
+// console.log(population)
+// console.log(isIsland)
+// console.log(language)
+
+// population++
+// console.log(population)
+
+// let newPop = population / 2
+// console.log(newPop)
+
+// let finland = 6
+// console.log(population < finland);
+// finland += 4
+// console.log(finland)
+    
+// const birth_year = 1991
+// console.log("Birth Year =", birth_year)
+
+// const description_1 = `I'm ${continent}, a ${country}`
+
+
+// console.log(description_1)
+
+// console.log(
+// `this
+// is a
+// muultiline
+// string`
+// )
+
+// if (finland > population) {
+//     console.log('True')
+// } else {
+//     console.log("Finland is not greater than Canada")
+// }
+
+// const birthYear = 2002;
+
+// let century;
+// if (birthYear <= 2000) {
+//     century = 20;
+// } else {
+//     century = 21;
+// }
+
+// console.log(century)
+
+// const age = 18
+// if (age === 18) console.log('Yay')
+
+// const fav = prompt("What's your favorite color?")
+// console.log(fav)
+
+// if (fav == 13) {
+//     console.log("Great Choice!")
+// } else {
+//     console.log("not that great!")
+// }
+
+// const hasDriversLicense = true;
+// const hasGoodVision = true;
+
+// console.log(hasDriversLicense && hasGoodVision)
+// console.log(hasDriversLicense || hasGoodVision)
+// console.log(!hasGoodVision)
+
+// const day = 'tuesday';
+
+// switch (day) {
+//     case 'monday':
+//         console.log('Plan course structure');
+//         console.log('Go to coding meetup');
+//         break;
+//     case 'tuesday':
+//         console.log('Prepare theory videos');
+//         break;
+//     case 'wednesday':
+//         console.log('Relax');
+//         break;
+//     case 'thursday':
+//         console.log('something else');
+//         break;
+//     case 'friday':
+//         console.log('another');
+//         break;
+//     case 'saturday':
+//         console.log('music');
+//         break;
+//     case 'sunday':
+//         console.log('clean');
+//         break;
+// }
+
+// if (day == 'monday') {
+//     console.log('Plan course structure');
+//     console.log('Go to coding meetup');
+// } else if (day == 'tuesday') {
+//     console.log('Prepare theory videos x2');
+// }
+
+let age = 17
+age >= 18 ? console.log('I like to drink wine') :
+    console.log('I like to drink water')
+
+const drink = age >= 18 ? 'wine' : 'water';
+console.log(drink)
+
+console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`);
